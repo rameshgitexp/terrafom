e2da77bce22b7459a74b0ce1e28560678f26169f
rameshgitexp <141246903+rameshgitexp@users.noreply.github.com>
2024-05-18 21:00:46 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"  # Specifies the source of the AWS provider
-      version = "~> 1.8.3"       # Specifies the version constraint for the AWS provider
+      version = "~> v1.8.3"       # Specifies the version constraint for the AWS provider
     }
   }
 }
