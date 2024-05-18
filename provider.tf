ceff38c77b54d8ff12d63c84b9068bfe0096ce29
rameshgitexp <141246903+rameshgitexp@users.noreply.github.com>
2024-05-18 21:05:19 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"  # Specifies the source of the AWS provider
-      version = "~> v1.8.3"       # Specifies the version constraint for the AWS provider
-    }
-  }
-}
-
 provider "aws" {
   region = var.region  # Specifies the AWS region using a variable
 }
